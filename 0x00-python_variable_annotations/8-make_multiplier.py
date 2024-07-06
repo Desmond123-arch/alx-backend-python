90101cab9b355fb98a95ba725f6c07a7e16e36b3
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-06 00:52:02 +0000

    done

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,8 @@ from typing import Callable
 
 
 def make_multiplier(factor: float) -> Callable[[float], float]:
+    """ Makes a multiplier """
     def multiplier(x: float) -> float:
+        """ Generate multiplier"""
         return x * factor
     return multiplier
