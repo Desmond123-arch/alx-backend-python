c50ea59c17b2f6ffd49bd55774817c5435d7a391
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-09 02:11:12 +0000

    awaitable generator

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Creating tasks """
 import asyncio
-from typing import Generator
+from typing import AwaitableGenerator
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Generator:
+def task_wait_random(max_delay: int) -> AwaitableGenerator:
     """ Create and return a task"""
     return asyncio.create_task(wait_random(max_delay))
