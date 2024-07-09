40c45c304352f216b12ffbc5f7528aef5ecb8cf0
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-09 01:32:53 +0000

    typings

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """ Execute multiple coroutines"""
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Calls wait_random n times"""
     vals = []
     tasks = []
