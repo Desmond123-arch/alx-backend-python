230b1f7af34e039147256d3531598669de60c5b1
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-10 00:59:22 +0000

    none a typing error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ An async generator """
 import asyncio
 import random
-from typing import Generator, AsyncGenerator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float]:
+async def async_generator() -> AsyncGenerator:
     for _ in range(10):
         await asyncio.sleep(1)
         yield (random.uniform(0, 10))
